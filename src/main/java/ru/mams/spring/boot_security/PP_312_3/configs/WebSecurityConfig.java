f783f5cbf1b5b663b97f6473f5ec168cda5d9480
Victoria2412 <nado122012@gmail.com>
2022-10-15 10:55:21 +0300

    2.1

--- src/main/java/ru/mams/spring/boot_security/PP_312_3/configs/WebSecurityConfig.java
+++ src/main/java/ru/mams/spring/boot_security/PP_312_3/configs/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().successHandler(successUserHandler)
                 .permitAll()
                 .and()
-                .logout().logoutSuccessUrl("/")
+                .logout().logoutSuccessUrl("/login")
                 .permitAll();
     }
 
